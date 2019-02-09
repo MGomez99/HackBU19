23317ce385e4b49d4c01181c3cd0e294107a2229
MGomez99 <xmgomez2012@gmail.com>
2019-02-09 15:44:01 -0500

    formatting and adding namespace

--- game.h
+++ game.h
@@ -10,25 +10,26 @@ enum Pieces {empty = -1, pawnW, rookW, knightW, bishopW, queenW, kingW, pawnB, r
     /*  White < 6 ; Black >= 6
         Pawn = 0/6, Rook = 1/7, Knight = 2/8, Bishop = 3/9, Queen = 4/10, King = 5/12
     */
-
-class game{
-    private:
-        std::vector <std::vector<int>> gameboard{ {rookB, knightB, bishopB, queenB, kingB, bishopB, knightB, rookB}, 
-                                        {pawnB, pawnB, pawnB, pawnB, pawnB, pawnB, pawnB, pawnB},
-                                        {empty, empty, empty, empty, empty, empty, empty, empty},
-                                        {empty, empty, empty, empty, empty, empty, empty, empty},
-                                        {empty, empty, empty, empty, empty, empty, empty, empty},
-                                        {empty, empty, empty, empty, empty, empty, empty, empty},
-                                        {pawnW, pawnW, pawnW, pawnW, pawnW, pawnW, pawnW, pawnW},
-                                        {rookW, knightW, bishopW, queenW, kingW, bishopW, knightW, rookW} };
-    public:
-        bool isValidMove(int piece);
-        bool isInCheck(int turn); //is the player in check; 0 = white | 1 = black
-        bool gameOver(); //is the game won 
-        void move(int piece, std::tuple<int, int> move); //move piece
-        std::vector <std::vector<int>> getBoard(){ return this->gameboard;}
+namespace game{
+    class gameboard{ 
+        private:
+            std::vector <std::vector<int>> gameboard{ {rookB, knightB, bishopB, queenB, kingB, bishopB, knightB, rookB}, 
+                                            {pawnB, pawnB, pawnB, pawnB, pawnB, pawnB, pawnB, pawnB},
+                                            {empty, empty, empty, empty, empty, empty, empty, empty},
+                                            {empty, empty, empty, empty, empty, empty, empty, empty},
+                                            {empty, empty, empty, empty, empty, empty, empty, empty},
+                                            {empty, empty, empty, empty, empty, empty, empty, empty},
+                                            {pawnW, pawnW, pawnW, pawnW, pawnW, pawnW, pawnW, pawnW},
+                                            {rookW, knightW, bishopW, queenW, kingW, bishopW, knightW, rookW} };
+        public:
+            bool isValidMove(int piece);
+            bool isInCheck(int turn); //is the player in check; 0 = white | 1 = black
+            bool gameOver(); //is the game won 
+            void move(int piece, std::tuple<int, int> move); //move piece
+            std::vector <std::vector<int>> getBoard(){ return this->gameboard;}
 
 
 };
+}
 
 #endif

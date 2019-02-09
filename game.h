45bd9b4a0eab76d6fc656f38b7c1bcb47f14f65c
Thomas Horowitz <thorowi1@binghamton.edu>
2019-02-09 16:31:27 -0500

    working on main

--- game.h
+++ game.h
@@ -3,7 +3,6 @@
 #include <vector>
 #include <cstdio>
 #include <string>
-#include <tuple>
 
 #ifndef GAME_1
 #define GAME_1
@@ -12,10 +11,10 @@ enum Pieces {empty = -1, pawnW, rookW, knightW, bishopW, queenW, kingW, pawnB, r
         Pawn = 0/6, Rook = 1/7, Knight = 2/8, Bishop = 3/9, Queen = 4/10, King = 5/12
     */
 namespace game{
-    class gameboard{ 
+    class gameboard{
         private:
-            std::vector <std::vector<int>> gameboard{ 
-                {rookB, knightB, bishopB, queenB, kingB, bishopB, knightB, rookB}, 
+            std::vector <std::vector<int>> gameboard{
+                {rookB, knightB, bishopB, queenB, kingB, bishopB, knightB, rookB},
                 {pawnB, pawnB, pawnB, pawnB, pawnB, pawnB, pawnB, pawnB},
                 {empty, empty, empty, empty, empty, empty, empty, empty},
                 {empty, empty, empty, empty, empty, empty, empty, empty},
@@ -24,9 +23,9 @@ namespace game{
                 {pawnW, pawnW, pawnW, pawnW, pawnW, pawnW, pawnW, pawnW},
                 {rookW, knightW, bishopW, queenW, kingW, bishopW, knightW, rookW} };
         public:
-            bool isValidMove(int piece, tuple<int> position);
+            bool isValidMove(int piece, std::tuple<int> position);
             bool isInCheck(int turn); //is the player in check; 0 = white | 1 = black
-            bool gameOver(); //is the game won 
+            bool gameOver(); //is the game won
             void move(int piece, std::tuple<int, int> move); //move piece
             std::vector <std::vector<int>> getBoard(){ return this->gameboard;}
 
--- main.cpp
+++ main.cpp
@@ -11,10 +11,13 @@ int main(){
   string space1;
   string space2;
   string delim = "-";
+  bool valid = false;
+  int current[2];
+  int new[2];
 
-  cout << "\n\n\nQuick explanation of how to input moves: put the name of the space the piece is on first and the space you want to move to second.\nexamples: d8-d6 or h7-h5\nMake sure everything is lowercase and you seperate the spaces with dashes\n\n\n" << endl;
-//  while(running){
-    bool valid = false;
+  cout << "\n\n\nQuick explanation of how to input moves: put the name of the space the piece is on \nfirst and the space you want to move to second.  Examples: d8-d6 or h7-h5\nMake sure everything is lowercase and you seperate the spaces with dashes\n\n\n" << endl;
+  while(running){
+    //white's turn
     while(!valid){
       valid = true;
       cout << "White, please select a move" << endl;
@@ -25,9 +28,7 @@ int main(){
 
       space1 = move.substr(0, move.find(delim));
       space2 = move.substr(move.find(delim)+1, move.length());
-      cout << space1 << endl;
-      cout << space2 << endl;
-      
+
       if(space1.length() != 2  || space2.length() != 2){
         valid = false;
       }
@@ -46,7 +47,46 @@ int main(){
       if(space1 == space2){
         valid = false;
       }
+      if(valid == false){
+        cout << "The move you have selected in not valid, please input a different move." << endl;
+      }
     }
 
-//  }
+
+    //Black's turn
+    valid = false;
+    while(!valid){
+      valid = true;
+      cout << "Black, please select a move" << endl;
+      cin >> move;
+      if(move.find(delim)  == -1){
+        valid = false;
+      }
+
+      space1 = move.substr(0, move.find(delim));
+      space2 = move.substr(move.find(delim)+1, move.length());
+
+      if(space1.length() != 2  || space2.length() != 2){
+        valid = false;
+      }
+      if(space1.front() < 97 ||  space1.front() > 104){
+        valid = false;
+      }
+      if(space2.front() < 97 ||  space2.front() > 104){
+        valid = false;
+      }
+      if(space1.back() < 49 || space1.back() > 56){
+        valid = false;
+      }
+      if(space2.back() < 49 || space2.back() > 56){
+        valid = false;
+      }
+      if(space1 == space2){
+        valid = false;
+      }
+      if(valid == false){
+        cout << "The move you have selected in not valid, please input a different move." << endl;
+      }
+    }
+  }
 }

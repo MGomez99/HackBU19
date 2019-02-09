a4c4759b4174f3b315117a032f72c1774bd41a39
Thomas Horowitz <32752723+tomwitz355@users.noreply.github.com>
2019-02-09 15:13:13 -0500

    fixed main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <cstdlib>
 #include <iostream>
 #include "game.h"
@@ -29,20 +28,3 @@ int main(){
 //  while(running){
 //  }
 }
-=======
-#include <cstdlib>
-#include <iostream>
-#include "game.h"
-
-using namespace std;
-
-int main(){
-  bool running = true;
-//  String move;
-  cout << "\n\n\nQuick explanation of how to input moves: put the first letter of the name of the piece\n(king = k, queen = q, rook = r, bishop = b, knight = k and no letter for pawn) and then \nput the name of the space the piece is on first and the space you want to move to second.\nexamples: qd8d6 or h7h5.  Make sure everything is lowercase!\n\n\n" << endl;
-//  while(running){
-//    cout << "White, please select a move" << endl;
-//    cin >> move;
-//  }
-}
->>>>>>> 8c0a7724f5756de62c202c71295ce4def2b44563
